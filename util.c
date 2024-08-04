e66da82459122b19c2797d58e1b164923c1eea5f
fkatsaras <katsarasfotis99@gmail.com>
2024-08-04 23:12:58 +0300

    dummy

--- util.c
+++ util.c
@@ -1,10 +1,88 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+typedef struct SparseMatrixCSR{
+
+    int *I_ptr;
+    int *J;
+    long double *val;
+
+    int M;
+    int N;
+    int nz;
+} SparseMatrixCSR;
+
+typedef struct SparseMatrixCOO
+{
+    /*         
+            N , J
+    +-------------------+
+    | *                 |
+    |    *    *         |
+    |    *  *           | M, I
+    |          *  *   * |
+    |             *     |
+    |      *   *     *  |
+    +-------------------+
+    
+    nnz = *
+    (I[nnz], J[nnz]) = value
+    
+    Legend:
+    M = Rows
+    N = Columns
+    I = Row indices
+    J = Column indices
+   */
+    int M, N, nnz; // Matrix size info
+
+    int *I, *J; // Matrix value info 
+    double *val
+} SparseMatrixCOO;
+
+// Function to initialize a sparse matrix
+void initSparseMatrix(SparseMatrixCOO *mat, int M, int N, int nnz) {
+
+    mat->M = M;
+    mat->N = N;
+    mat->nnz = nnz;
+    
+    mat->I = (int *)malloc(nnz * sizeof(int));
+    mat->J = (int *)malloc(nnz * sizeof(int));
+    mat->val = (double *)malloc(nnz * sizeof(double));
+}
+
+// Function to free the memory allocated for a Sparse Matrix
+void freeSparseMatrix(SparseMatrixCOO *mat) {
+    free(mat->I);
+    free(mat->J);
+    free(mat->val);
+}
+
+// Function to print a sparse matrix in COO format
+void printSparseMatrix(SparseMatrixCOO *mat, bool full) {
+    printf(" SparseMatrixCOO(shape = ( %d , %d ), nnz = %d )\n", mat->M, mat->N, mat->nnz );
+    if (full) {
+        for (int i = 0; i < mat->nnz; i++) {
+            printf("\t( %d , %d ) = %f\n", mat->I[i], mat->J[i], mat->val[i] );
+        }
+    }
+}
+
+int readSparseMatrix(const char *filename, SparseMatrixCOO *mat) {
+    int ret = mm_read_unsymmetric_sparse(filename, &mat->M, &mat->N, &mat->nnz, &mat->val, &mat->I, &mat->J);
+    if (ret!=0) {
+        fprintf(stderr, "\tFailed to read the matrix from file %s\n", filename);
+        return ret;
+    }
+    return 0;
+}
 
 // Converts COO to CSR
-struct CSR COOtoCSR(struct COO A){
+SparseMatrixCSR COOtoCSR(SparseMatrixCOO  A){
 
-    struct CSR output;
-    output.nz = A.nz;
+    SparseMatrixCSR output;
+    output.nz = A.nnz;
     output.M = A.M;
     output.N = A.N;
 
@@ -16,7 +94,7 @@ struct CSR COOtoCSR(struct COO A){
         output.I_ptr[i] = 0;
     }
 
-    for (int i = 0; i < A.nz; i++){
+    for (int i = 0; i < A.nnz; i++){
         output.val[i] = A.val[i];
         output.J[i] = A.J[i];
         output.I_ptr[A.I[i] + 1]++;
